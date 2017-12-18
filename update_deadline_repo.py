66bbeeb0e185ec9499ec5e4e56938f44ad96f9d7
Matt <matt.carroll@imaginaryforces.com>
2017-12-18 16:41:16 -0500

    test / add update_deadline_repo.py to repo

---
+++ update_deadline_repo.py
@@ -0,0 +1,12 @@
+# I created a new deadline_repo for Deadline8. The nodes look for this repo using the 
+# deadline.ini file. This script removes the old file and replaces with updated version.
+# Written by Matt Carroll
+
+import os
+import shutil
+
+## Remove deadline config file
+os.remove("C:\ProgramData\Thinkbox\Deadline8\deadline.ini")
+
+## Copy NY Repository config file from Server to destination 
+shutil.copy("T:\_Systems\matt\deadline.ini", "C:\ProgramData\Thinkbox\Deadline8")
