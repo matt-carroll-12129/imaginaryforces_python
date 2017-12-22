c69b91813665da69a27a9a23125530e637ea0632
Matt <matt.carroll@imaginaryforces.com>
2017-12-22 12:36:00 -0500

    test / add osx_nuke_update.py to repo

---
+++ osx_nuke_update.py
@@ -0,0 +1,19 @@
+import os 
+import shutil
+
+## Remove FLEXLM files from 2 locations
+pathFLEXfolder = "/Library/Application Support/TheFoundry/FLEXlm"
+if os.path.exists(pathFLEXfolder):
+	fileList = os.listdir(pathFLEXfolder)
+	for fileName in fileList:
+		os.remove(pathFLEXfolder + "/" + fileName)
+
+
+## Ensure the RLM directory exists. If it does not, create one. 
+if not os.path.exists("/Library/Application Support/TheFoundry/RLM"):
+	os.makedirs("/Library/Application Support/TheFoundry/RLM")
+
+## Copy the client .lic file into RLM folder. This points client to primary and failover server
+if not os.path.exists("/Library/Application Support/TheFoundry/RLM/foundry_client.lic"):
+	shutil.copy("/Romulus/romulus/_Systems/licenses/nuke/for_client/foundry_client.lic", "/Library/Application Support/TheFoundry/RLM/")
+
