c9c66addba99a9496d555327a918183221059160
Matt <matt.carroll.12129@gmail.com>
2018-01-02 14:31:55 -0500

    fixed typo

--- pathswap.py
+++ pathswap.py
@@ -1,4 +1,4 @@
-# This is function but could use some polishing. It is for my own use to swap Nuke file paths and is not out in production. It is meant to be run from command line and takes inputs of 
+# This is functional but could use some polishing. It is for my own use to swap Nuke file paths and is not out in production. It is meant to be run from command line and takes inputs of 
 # a nuke file directory and nuke file name. It will create a new file in which paths are swapped from unix to Windows.
 # The new file has "_windows.nk" added to the end. 
 # Written by Matt Carroll
